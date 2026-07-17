9f7bf10f3c79b47a32a66e7159f77d65be3b05ea
Gregory <gregorysantos575@gmail.com>
2026-07-17 16:59:50 -0300

    exercicios

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  text: string = '';
+
+  nome: string = '';
+
+}
